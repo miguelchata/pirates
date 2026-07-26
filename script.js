5b519df71c4dbf42245edd5e8484c79b7f6b426e
Miguel Chata <mic.siu7@gmail.com>
2026-07-26 14:25:32 -0500

    Code improved with mantainability and scalability

--- script.js
+++ script.js
@@ -1,59 +1,48 @@
-// in Millions
 const RESOURCES = {
-	food: { quantity: 30, price: 350, compress: 1000},
-	wood: { quantity: 5, price: 350, compress: 170},
-	silver: { quantity: 1.25, price: 350, compress: 42},
+	food: { amount: 0, gold: 0, compress: '', quantity: 30, price: 350, one: 1000},
+	wood: { amount: 0, gold: 0, compress: '', quantity: 5, price: 350, one: 170},
+	silver: { amount: 0, gold: 0, compress: '', quantity: 1.25, price: 350, one: 42},
 }
 
-const button = document.getElementById("button-calculate")
+const UI = {
+	calcButton: document.getElementById("button-calculate"),
+	goldInput: document.getElementById("gold"),
+	foodInput: document.getElementById("food"),
+	woodInput: document.getElementById("wood"),
+	silverInput: document.getElementById("silver"),
+	foodResult: document.getElementById("food-result"),
+	woodResult: document.getElementById("wood-result"),
+	silverResult: document.getElementById("silver-result"),
+	goldResult: document.getElementById("gold-result"),
+}
 
-button.addEventListener('click', calculateResource);
-
-
-function calculateResource(argument) {
-	const goldInput = document.getElementById("gold")
-	const goldValue = Number(goldInput.value)
-
-	const foodInput = document.getElementById("food");
-	const woodInput = document.getElementById("wood");
-	const silverInput = document.getElementById("silver");
-
-	const foodResult = document.getElementById("food-result")
-	const woodResult = document.getElementById("wood-result")
-	const silverResult = document.getElementById("silver-result")
-	const goldResult = document.getElementById("gold-result")
-
-	const foodValue = Number(foodInput.value);
-	const woodValue = Number(woodInput.value);
-	const silverValue = Number(silverInput.value);
+UI.calcButton.addEventListener('click', calculateResource);
 
 
+function calculateResource() {
+	const goldValue = Number(UI.goldInput.value)
+
+	for (const [key, value] of Object.entries(RESOURCES)) {
+		const inputValue = Number(UI[`${key}Input`].value)
+
+		RESOURCES[key].amount = inputValue * RESOURCES[key].quantity;
+		RESOURCES[key].gold = inputValue * RESOURCES[key].price;
+		RESOURCES[key].compress = convertToCompressed(RESOURCES[key].amount, RESOURCES[key].one);
+
+	}
+
+	renderResults();
+}
+
+function renderResults() {
 	let totalGold = 0;
 
-	// calculate foods
-	let foodQuantity = foodValue * RESOURCES.food.quantity;
-	let foodGoldCost = foodValue * RESOURCES.food.price;
+	for (const [key, value] of Object.entries(RESOURCES)) {
+		UI[`${key}Result`].innerText = `Amount: ${value.amount}M - Gold: ${convertBigNumber(value.gold)} Compressed: ${value.compress}`;
+		totalGold += value.gold;
+	}
 
-	// calculate woods
-	let woodQuantity = woodValue * RESOURCES.wood.quantity;
-	let woodGoldCost = woodValue * RESOURCES.wood.price;
-
-	// calculate silvers
-	let silverQuantity = silverValue * RESOURCES.silver.quantity;
-	let silverGoldCost = silverValue * RESOURCES.silver.price;
-
-	totalGold = foodGoldCost + woodGoldCost + silverGoldCost;
-
-
-	let foodCompressed = convertToCompressed(foodQuantity, RESOURCES.food.compress);
-	let woodCompressed = convertToCompressed(woodQuantity, RESOURCES.wood.compress);
-	let silverCompressed = convertToCompressed(silverQuantity, RESOURCES.silver.compress);
-
-	goldResult.innerText = `Total Gold: ${convertBigNumber(totalGold)}`;
-	foodResult.innerText = `Quantity: ${foodQuantity}M - Gold: ${convertBigNumber(foodGoldCost)} Compressed: ${foodCompressed}`;
-	woodResult.innerText = `Quantity: ${woodQuantity}M - Gold: ${convertBigNumber(woodGoldCost)} Compressed: ${woodCompressed}`;
-	silverResult.innerText = `Quantity: ${silverQuantity}M - Gold: ${convertBigNumber(silverGoldCost)} Compressed: ${silverCompressed}`;
-
+	UI.goldResult.innerText = `Total Gold: ${convertBigNumber(totalGold)}`;
 }
 
 
@@ -77,5 +66,5 @@ function convertToCompressed(resource, rate) {
 	let resourceLeft = (resource * 1000) % rate;
 	let resourceCompressed = ((resource * 1000 ) - resourceLeft) / rate;
 
-	return `${resourceCompressed}/${resourceLeft}K`
+	return `${resourceCompressed}units / ${resourceLeft}K`
 }
