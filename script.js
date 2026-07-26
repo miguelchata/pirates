f3c552cc728dd6f2222ee313dfc68f59767eff2f
Miguel Chata <mic.siu7@gmail.com>
2026-07-26 18:46:26 -0500

    Code improved with clear logic

--- script.js
+++ script.js
@@ -1,48 +1,95 @@
-const RESOURCES = {
-	food: { amount: 0, gold: 0, compress: '', quantity: 30, price: 350, one: 1000},
-	wood: { amount: 0, gold: 0, compress: '', quantity: 5, price: 350, one: 170},
-	silver: { amount: 0, gold: 0, compress: '', quantity: 1.25, price: 350, one: 42},
+const calcButton = document.getElementById("button-calculate");
+
+calcButton.addEventListener('click', clickHandler);
+
+const RESOURCES = Object.freeze({
+	food: { quantity: 30, price: 350, compressionRate: 1000},
+	wood: { quantity: 5, price: 350, compressionRate: 170},
+	silver: { quantity: 1.25, price: 350, compressionRate: 42},
+	gold: { quantity: 0, price: 0, compressionRate: 0 },
+});
+
+const UI = collectUI();
+
+function clickHandler() {
+	clearUI();
+	const values = collectValues();
+
+	const { data, errors } = calculateResource(values);
+
+	renderErrors(errors);
+	renderResults(data);
 }
 
-const UI = {
-	calcButton: document.getElementById("button-calculate"),
-	goldInput: document.getElementById("gold"),
-	foodInput: document.getElementById("food"),
-	woodInput: document.getElementById("wood"),
-	silverInput: document.getElementById("silver"),
-	foodResult: document.getElementById("food-result"),
-	woodResult: document.getElementById("wood-result"),
-	silverResult: document.getElementById("silver-result"),
-	goldResult: document.getElementById("gold-result"),
+function collectUI() {
+	let elements = {};
+
+	for (const key of Object.keys(RESOURCES)) {
+		elements[key] = document.getElementById(key);
+		elements[`${key}-result`] = document.getElementById(`${key}-result`);
+		elements[`${key}-error`] = document.getElementById(`${key}-error`);
+	}
+
+	return elements;
 }
 
-UI.calcButton.addEventListener('click', calculateResource);
+function collectValues() {
+	let values = {};
+
+	for (let input of Object.keys(RESOURCES)) {
+		values[input] = UI[input].value;
+	}
+
+	return values;
+}
 
 
-function calculateResource() {
-	const goldValue = Number(UI.goldInput.value)
+function calculateResource(values) {
+	let result = {
+		data: {},
+		errors: [],
+	};
 
 	for (const [key, value] of Object.entries(RESOURCES)) {
-		const inputValue = Number(UI[`${key}Input`].value)
+		const inputValue = Number(values[key])
 
-		RESOURCES[key].amount = inputValue * RESOURCES[key].quantity;
-		RESOURCES[key].gold = inputValue * RESOURCES[key].price;
-		RESOURCES[key].compress = convertToCompressed(RESOURCES[key].amount, RESOURCES[key].one);
+		if (!Number.isNaN(inputValue) && Number.isFinite(inputValue) && inputValue > 0) {
+			const amount = inputValue * value.quantity;
+			const gold = inputValue * value.price;
+			const compress = convertToCompressed(amount, value.compressionRate); 
+
+			result.data[key] = { amount, gold, compress };
+		} else {
+			result.errors.push(key);
+		}
 
 	}
 
-	renderResults();
+	return result;
 }
 
-function renderResults() {
+function clearUI() {
+	for (const key of Object.keys(RESOURCES)) {
+		UI[`${key}-result`].innerText = "";
+		UI[`${key}-error`].innerText = "";
+	}
+}
+
+function renderErrors(errors) {
+	for (let error of errors ) {
+		UI[`${error}-error`].innerText = "Input should be a number and greater than zero!";
+	}
+}
+
+function renderResults(results) {
 	let totalGold = 0;
 
-	for (const [key, value] of Object.entries(RESOURCES)) {
-		UI[`${key}Result`].innerText = `Amount: ${value.amount}M - Gold: ${convertBigNumber(value.gold)} Compressed: ${value.compress}`;
+	for (const [key, value] of Object.entries(results)) {
+		UI[`${key}-result`].innerText = `Amount: ${value.amount}M - Gold: ${convertBigNumber(value.gold)} Compressed: ${value.compress}`;
 		totalGold += value.gold;
 	}
 
-	UI.goldResult.innerText = `Total Gold: ${convertBigNumber(totalGold)}`;
+	UI[`gold-result`].innerText = `Total Gold: ${convertBigNumber(totalGold)}`;
 }
 
 
