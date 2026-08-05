3f515e4915b871ff99f552173c206c62621359e7
Miguel Chata <mic.siu7@gmail.com>
2026-08-05 17:25:39 -0500

    add for string validation

--- validators.js
+++ validators.js
@@ -3,5 +3,10 @@ export const validatros = {
 		if (typeof value !== "number" || Number.isNaN(value)) {
 			return "Must be a number";
 		}
+	},
+	string: (value, ruel) => {
+		if (typeof value !== "string") {
+			return "Must be a valid string.";
+		}
 	}
 }
